083184c96882624fab1514dff55c3ad93c15d1dd
mahmudmustafayev2009-droid <mahmudmustafayev2009@gmail.com>
2026-05-15 19:48:20 +0400

    new task

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello world')
+a = int(input())
+
+say = 0
+for i in range(2,a//2+1):
+    if a%i==0:
+        say+=1
+        print('sade deyil')
+        
+if say==0:
+    print('sadedir')
